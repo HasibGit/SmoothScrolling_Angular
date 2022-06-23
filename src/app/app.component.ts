c783a742b63359c84d6ad8aab5ec17007c00870b
Hasib Ullah <hasibullah04@gmail.com>
2022-06-23 13:56:59 +0600

    Successfully implemented smooth scroll effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { OnInit, VERSION } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'SmoothScroll';
+
+  name = 'Angular ' + VERSION.major;
+
+  constructor(private scroller: ViewportScroller, private router: Router) {}
+  ngOnInit() {
+    this.router.navigate(['/']);
+  }
+
+  goDown1() {
+    this.scroller.scrollToAnchor('targetRed');
+  }
+
+  goDown2() {
+    //this.scroller.scrollToAnchor("targetGreen");
+    document.getElementById('targetGreen').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+  }
+
+  goDown3() {
+    this.router.navigate([], { fragment: 'targetBlue' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+];
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+  //scrollPositionRestoration: "enabled"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HelloComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes, routerOptions),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
